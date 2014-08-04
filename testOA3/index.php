6c4a47d9da3853e1be90491ef7bd9179a5046483
xfsds <xfsds@qq.com>
2014-08-04 14:44:35 +0800

    ds modified

---
+++ testOA3/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lan="zh-cn">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="OA System Alpha 3">
+    <meta name="author" content="DS">
+    <link rel="icon" href="favicon.ico">
+	<title>OA System Alpha 3</title>
+
+	<link rel="stylesheet" type="text/css" href="../bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../jquery-ui/1.11.0/jquery-ui.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/dataTables.bootstrap.css">
+
+	<script src="../js/jquery-1.11.1.min.js"></script>
+	<script src="../bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="../jquery-ui/1.11.0/jquery-ui.min.js"></script>
+	<script src="../js/jquery.dataTables.min.js"></script>
+	<script src="../js/dataTables.bootstrap.js"></script>
+
+	<script type="text/javascript" charset="utf-8">
+//		$(document).ready(function(){
+//			$('#table1').dataTable();
+//		});
+	</script>
+</head>
+
+<body>
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">AR OA System</a>
+			</div>
+			<div class="collapse navbar-collapse" id="navbar1">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="">首页</a></li>
+					<li class="dropdown">
+						<a href="" class="dropdown-toggle" data-toggle="dropdown">业务申请<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li class="dropdown-header">融资担保</li>
+							<li><a href="">个人融资担保业务</a></li>
+							<li><a href="">公司融资担保业务</a></li>
+							<li class="divider"></li>
+							<li class="dropdown-header">诉讼担保</li>
+							<li><a href="">个人诉讼担保</a></li>
+							<li><a href="">公司诉讼担保</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="" class="dropdown-toggle" data-toggle="dropdown">业务管理<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="">业务查询</a></li>
+							<li><a href="">资料提交</a></li>
+							<li><a href="">业务审批</a></li>
+						</ul>
+					</li>
+					<li><a href="">客户资料</a></li>
+					<li><a href="">担保品管理</a></li>					
+					<li><a href="">贷后调查</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="" class="dropdown-toggle" data-toggle="dropdown">个人<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="">修改密码</a></li>
+							<li><a href="">个人首页</a></li>
+							<li class="divider"></li>
+							<li><a href="">登出</a></li>
+						</ul>
+					</li>					
+					<li><a href="">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div style="min-height:2000px">
+	</div>
+	<div id="test1">
+	</div>
+
+
+	
+	<div class="container">
+		<footer>
+			<p align="center">&copy; Copyright 2014 Anti-Radiation Co., Ltd., All Rights Reserved.</p>
+		</footer>
+	</div>
+</body>
+
+</html>
